90ef16ba4c4d464eaf8533bcc821046e6361bf10
scrameee <itsxomyak@gmail.com>
2025-09-18 23:39:36 +0500

    feat: update models in scamdetector | need to init svc for this

--- internal/modules/scamdetector/model.go
+++ internal/modules/scamdetector/model.go
@@ -1,18 +1,34 @@
 package scamdetector
 
 type ScamDetectorRes struct {
-	TotalPercent    float64
-	DomainAge       string
-	DomainDate      string
-	Blacklist       string
-	Https           bool
-	ProximityToScam bool
-	Description     string //div about-text
-	Country         string
-	CountryCode     string
-	Email           string
-	SslDate         string
-	SslIssuer       string
-	ServerName      string
-	Review          string
+	// ===== Technical Analysis =====
+	DomainAge string `json:"Domain age"`
+
+	CompanyData string `json:"Company Data"`
+	Website     string `json:"Website"`
+	SSLValid    string `json:"SSL certificate valid"`
+	SSLIssuer   string `json:"SSL issuer"`
+	WHOISReg    string `json:"WHOIS registration date"`
+	WHOISUpd    string `json:"WHOIS last update date"`
+	WHOISRenew  string `json:"WHOIS renew date"`
+
+	Owner            string `json:"Owner"`
+	Administrator    string `json:"Administrator"`
+	TechnicalContact string `json:"Technical Contact"`
+
+	RegistrarName    string `json:"Name"`
+	RegistrarIanaID  string `json:"IANA ID"`
+	RegistrarWebsite string `json:"Register website"`
+	RegistrarEmail   string `json:"E-mail"`
+	RegistrarPhone   string `json:"Phone"`
+
+	ServerName []string `json:"Server Name"`
+
+	// ===== Summary =====
+	TotalPercent    string `json:"totalPercent"`
+	DomainAgeSum    string `json:"domainAge"`
+	DomainDate      string `json:"domainDate"`
+	BlackList       string `json:"blackList"`
+	HttpsConnection string `json:"httpsConnection"`
+	SiteDescription string `json:"siteDescription"`
 }
